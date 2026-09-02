2734da96eaf1e0a207d7c79156dc6568aa860bfa
Aditya Yadav <adityafree45638@gmail.com>
2026-09-02 16:23:52 +0530

    Time: 0 ms (100%), Space: 20.4 MB (67.36%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     int minDeletions(string s) {
         vector<int> freq(26, 0);
-
-        // Count frequencies
         for (char c : s) {
             freq[c - 'a']++;
         }
-
-        unordered_set<int> used;
+         unordered_set<int> used;
         int deletions = 0;
-
         for (int f : freq) {
             while (f > 0 && used.count(f)) {
                 f--;
                 deletions++;
             }
-
             if (f > 0) {
                 used.insert(f);
             }
         }
-
         return deletions;
     }
 };
