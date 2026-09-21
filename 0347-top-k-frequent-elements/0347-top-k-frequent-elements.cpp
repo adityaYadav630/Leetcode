20b29b3f26a409c13c021ad0eceba10fe8039c7e
Aditya Yadav <adityafree45638@gmail.com>
2026-09-21 21:12:00 +0530

    Time: 3 ms (51.65%), Space: 18 MB (53.3%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -5,12 +5,13 @@ public:
         for(auto &a:nums){
             m[a]++;
         }
-        priority_queue<pair<int,int>>p;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
         for(auto &a:m){
             p.push({a.second,a.first});
+            if(p.size()>k)p.pop();
         }
         vector<int>ans;
-        for(int i=1;i<=k;i++){
+        while(p.size()){
             ans.push_back(p.top().second);
             p.pop();
         }
